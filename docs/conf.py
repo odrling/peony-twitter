872e6302e91b9fc6f6f65f91606204d8390df5b0
odrling <odrling@gmail.com>
2017-02-23 17:21:04 +0100

    change some configuration options for the documentation

--- docs/conf.py
+++ docs/conf.py
@@ -73,7 +73,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'Peony'
-copyright = '2016, odrling'
+copyright = '2016-2017, Florian Badie'
 author = 'odrling'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -194,7 +194,7 @@ if not on_rtd:  # only import and set the theme if we're building docs locally
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied

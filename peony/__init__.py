52dbad595f3c8c53cb097361b6eff88f2d7315af
odrling <odrling@gmail.com>
2018-02-25 15:39:10 +0100

    v1.0.4

--- peony/__init__.py
+++ peony/__init__.py
@@ -14,7 +14,7 @@ __author__ = "Florian Badie"
 __author_email__ = "florianbadie@gmail.com"
 __url__ = "https://github.com/odrling/peony-twitter"
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 
 __license__ = "MIT License"
 

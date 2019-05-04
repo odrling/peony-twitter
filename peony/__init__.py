b2659ecd9cddf21b08ab23706fd184cccca96979
odrling <florianbadie@odrling.xyz>
2019-05-04 17:36:17 +0200

    version bump

--- peony/__init__.py
+++ peony/__init__.py
@@ -12,7 +12,7 @@ __author__ = "Florian Badie"
 __author_email__ = "florianbadie@gmail.com"
 __url__ = "https://github.com/odrling/peony-twitter"
 
-__version__ = "1.1.2"
+__version__ = "1.1.3"
 
 __license__ = "MIT License"
 

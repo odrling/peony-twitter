f973707ea615e16b56a6615925277d0d564a2ece
odrling <florianbadie@odrling.xyz>
2020-07-25 16:55:30 +0200

    update exceptions with new error codes #32
    Adds new error codes listed on
    https://developer.twitter.com/en/docs/basics/response-codes.
    
    Now error codes inherit from the exception for the status code, this
    should prevent code that relied on the status code exception from
    breaking with the next release but the order of the except clauses will
    matter (status code error should probably be catched after error codes
    exceptions).
    
    I should probably have done this a long time ago, because now this could
    also break some existing code.
    
    Will probably bump the major version number after this change in hope
    that it raises some eyebrows for someone that would see the update and
    wonder what changed.

--- peony/exceptions.py
+++ peony/exceptions.py
@@ -102,140 +102,6 @@ statuses = ErrorDict()
 errors = ErrorDict()
 
 
-@errors.code(32)
-class NotAuthenticated(PeonyException):
-    pass
-
-
-@errors.code(34)
-class DoesNotExist(PeonyException):
-    pass
-
-
-@errors.code(64)
-class AccountSuspended(PeonyException):
-    pass
-
-
-@errors.code(68)
-class MigrateToNewAPI(PeonyException):
-    pass
-
-
-@errors.code(88)
-class RateLimitExceeded(PeonyException):
-    """ Exception raised on rate limit """
-
-    @property
-    def reset(self):
-        """
-            Time when the limit will be reset
-
-        Returns
-        -------
-        int
-            Time when the limit will be reset
-        """
-        return int(self.response.headers.get('X-Rate-Limit-Reset', 0))
-
-    @property
-    def reset_in(self):
-        """
-            Time in seconds until the limit will be reset
-
-        Returns
-        -------
-        int
-            Time in seconds until the limit will be reset
-        """
-        return max(self.reset - time(), 0)
-
-
-@errors.code(92)
-class SSLRequired(PeonyException):
-    pass
-
-
-@errors.code(130)
-class OverCapacity(PeonyException):
-    pass
-
-
-@errors.code(131)
-class InternalError(PeonyException):
-    pass
-
-
-@errors.code(135)
-class CouldNotAuthenticate(PeonyException):
-    pass
-
-
-@errors.code(136)
-class Blocked(PeonyException):
-    pass
-
-
-@errors.code(161)
-class FollowLimit(PeonyException):
-    pass
-
-
-@errors.code(179)
-class ProtectedTweet(PeonyException):
-    pass
-
-
-@errors.code(185)
-class StatusLimit(PeonyException):
-    pass
-
-
-@errors.code(187)
-class DuplicatedStatus(PeonyException):
-    pass
-
-
-@errors.code(215)
-class BadAuthentication(PeonyException):
-    pass
-
-
-@errors.code(226)
-class AutomatedRequest(PeonyException):
-    pass
-
-
-@errors.code(231)
-class VerifyLogin(PeonyException):
-    pass
-
-
-@errors.code(251)
-class RetiredEndpoint(PeonyException):
-    pass
-
-
-@errors.code(261)
-class ReadOnlyApplication(PeonyException):
-    pass
-
-
-@errors.code(271)
-class CannotMuteYourself(PeonyException):
-    pass
-
-
-@errors.code(272)
-class NotMutingUser(PeonyException):
-    pass
-
-
-@errors.code(354)
-class DMCharacterLimit(PeonyException):
-    pass
-
-
 @statuses.code(304)
 class NotModified(PeonyException):
     pass
@@ -266,13 +132,13 @@ class NotAcceptable(PeonyException):
     pass
 
 
-@statuses.code(410)
-class Gone(PeonyException):
+@statuses.code(409)
+class Conflict(PeonyException):
     pass
 
 
-@statuses.code(415)
-class UnsupportedMediaType(PeonyException):
+@statuses.code(410)
+class Gone(PeonyException):
     pass
 
 
@@ -308,4 +174,314 @@ class ServiceUnavailable(PeonyException):
 
 @statuses.code(504)
 class GatewayTimeout(PeonyException):
+    pass
+
+
+@errors.code(3)
+class InvalidCoordinates(statuses[400]):
+    pass
+
+
+@errors.code(13)
+class NoLocationAssociatedToIP(statuses[404]):
+    pass
+
+
+@errors.code(17)
+class NoUserMatchesQuery(statuses[404]):
+    pass
+
+
+@errors.code(32)
+class NotAuthenticated(statuses[401]):
+    pass
+
+
+@errors.code(34)
+class DoesNotExist(statuses[404]):
+    pass
+
+
+@errors.code(36)
+class CannotReportYourselfAsSpam(statuses[403]):
+    pass
+
+
+@errors.code(38)
+class ParameterMissing(statuses[403]):
+    pass
+
+
+@errors.code(44)
+class AttachmentURLInvalid(statuses[400]):
+    pass
+
+
+@errors.code(50)
+class UserNotFound(statuses[404]):
+    pass
+
+
+@errors.code(63)
+class UserSuspended(statuses[404]):
+    pass
+
+
+@errors.code(64)
+class AccountSuspended(statuses[403]):
+    pass
+
+
+@errors.code(68)
+class MigrateToNewAPI(statuses[410]):
+    pass
+
+
+@errors.code(87)
+class ActionNotPermitted(statuses[403]):
+    pass
+
+
+# TODO: check if that could be moved to RateLimitExceeded
+@errors.code(88)
+class RateLimitExceeded(statuses[429]):
+    """ Exception raised on rate limit """
+
+    @property
+    def reset(self):
+        """
+            Time when the limit will be reset
+
+        Returns
+        -------
+        int
+            Time when the limit will be reset
+        """
+        return int(self.response.headers.get('X-Rate-Limit-Reset', 0))
+
+    @property
+    def reset_in(self):
+        """
+            Time in seconds until the limit will be reset
+
+        Returns
+        -------
+        int
+            Time in seconds until the limit will be reset
+        """
+        return max(self.reset - time(), 0)
+
+
+@errors.code(89)
+class InvalidOrExpiredToken(statuses[403]):
+    pass
+
+
+@errors.code(92)
+class SSLRequired(statuses[403]):
+    pass
+
+
+@errors.code(93)
+class ApplicationNotAllowedToAccessDirectMessages(statuses[403]):
+    pass
+
+
+@errors.code(99)
+class UnableToVerifyCredentials(statuses[403]):
+    pass
+
+
+@errors.code(120)
+class ValueTooLong(statuses[403]):
+    pass
+
+
+@errors.code(130)
+class OverCapacity(statuses[503]):
+    pass
+
+
+@errors.code(131)
+class InternalError(statuses[500]):
+    pass
+
+
+@errors.code(135)
+class CouldNotAuthenticate(statuses[401]):
+    pass
+
+
+@errors.code(139)
+class StatusAlreadyFavorited(statuses[403]):
+    pass
+
+
+@errors.code(144)
+class StatusNotFound(statuses[404]):
+    pass
+
+
+@errors.code(150)
+class CannotSendMessageToNonFollowers(statuses[403]):
+    pass
+
+
+@errors.code(160)
+class FollowRequestAlreadyChanged(statuses[403]):
+    pass
+
+
+@errors.code(161)
+class FollowLimit(statuses[403]):
+    pass
+
+
+@errors.code(179)
+class ProtectedTweet(statuses[403]):
+    pass
+
+
+@errors.code(185)
+class StatusLimit(statuses[403]):
+    pass
+
+
+@errors.code(186)
+class TweetTooLong(statuses[403]):
+    pass
+
+
+@errors.code(187)
+class DuplicatedStatus(statuses[403]):
+    pass
+
+
+@errors.code(205)
+class SpamReportLimit(statuses[403]):
+    pass
+
+
+@errors.code(214)
+class OwnerMustAllowDMFromAnyone(statuses[403]):
+    pass
+
+
+@errors.code(215)
+class BadAuthentication(statuses[400]):
+    pass
+
+
+@errors.code(220)
+class AccessNotAllowedByCredentials(statuses[403]):
+    pass
+
+
+@errors.code(226)
+class AutomatedRequest(statuses[403]):
+    pass
+
+
+@errors.code(251)
+class RetiredEndpoint(statuses[410]):
+    pass
+
+
+@errors.code(261)
+class ReadOnlyApplication(statuses[403]):
+    pass
+
+
+@errors.code(271)
+class CannotMuteYourself(statuses[403]):
+    pass
+
+
+@errors.code(272)
+class NotMutingUser(statuses[403]):
+    pass
+
+
+@errors.code(323)
+class GIFNotAllowedWithMultipleImages(statuses[400]):
+    pass
+
+
+@errors.code(324)
+class MediaIDValidationFailed(statuses[400]):
+    pass
+
+
+@errors.code(325)
+class MediaIDNotFound(statuses[400]):
+    pass
+
+
+@errors.code(326)
+class AccountLocked(statuses[403]):
+    pass
+
+
+@errors.code(327)
+class AlreadyRetweeted(statuses[403]):
+    pass
+
+
+@errors.code(349)
+class CannotSendMessageToUser(statuses[403]):
+    pass
+
+
+@errors.code(354)
+class DMCharacterLimit(statuses[403]):
+    pass
+
+
+@errors.code(355)
+class SubscriptionAlreadyExists(statuses[409]):
+    pass
+
+
+@errors.code(385)
+class ReplyToUnavailableTweet(statuses[403]):
+    pass
+
+
+@errors.code(386)
+class TooManyAttachmentTypes(statuses[403]):
+    pass
+
+
+@errors.code(407)
+class InvalidURL(statuses[400]):
+    pass
+
+
+@errors.code(415)
+class CallbackURLNotApproved(statuses[403]):
+    pass
+
+
+@errors.code(416)
+class InvalidOrSuspendedApplication(statuses[401]):
+    pass
+
+
+@errors.code(417)
+class DesktopApplicationAuth(statuses[401]):
+    pass
+
+
+@errors.code(421)
+class TweetNoLongerAvailable(statuses[404]):
+    pass
+
+
+@errors.code(422)
+class TweetViolatedRules(TweetNoLongerAvailable):
+    pass
+
+
+@errors.code(433)
+class TweetIsReplyRestricted(statuses[403]):
     pass

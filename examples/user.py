0db40bf1456caed6b00151a9276f1ab3e42fbd66
odrling <florianbadie@odrling.xyz>
2021-12-25 17:19:31 +0100

    add example that prints user data

---
+++ examples/user.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import asyncio
+
+try:
+    from . import api, peony
+except (SystemError, ImportError):
+    import api
+    from __init__ import peony
+
+
+async def search_test():
+    async with peony.PeonyClient(**api.keys) as client:
+        print(await client.user)
+
+if __name__ == '__main__':
+    asyncio.run(search_test())
